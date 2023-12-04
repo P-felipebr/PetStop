e7cbc364fa4d2eeea5c7d8cb6664130e03061744
P_felipebr <pfelope@gmail.com>
2023-12-04 00:10:56 -0300

    novos produtos funcionando

--- SCRIPT/carrosselHome.js
+++ SCRIPT/carrosselHome.js
@@ -1,16 +1,18 @@
-/* 
+
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 10,
     loop: true,
     grabCursor: true,
-   autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    }, 
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+        dynamicBullets: true,
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
     },
     navigation: {
         nextEl: ".swiper-button-next",
@@ -26,46 +28,55 @@ var swiper = new Swiper(".mySwiper", {
             spaceBetween: 40,
         },
         1024: {
-            slidesPerView: 5,
+            slidesPerView: 3,
             spaceBetween: 50,
-          },
+        },
+        1900: {
+            slidesPerView: 4,
+            spaceBetween: 80,
+        },
     },
-}); */
+});
 
-var swiper = new Swiper(".mySwiper", {
+
+var swiper = new Swiper(".novosProdutos-swiper", {
     slidesPerView: 1,
     spaceBetween: 10,
     loop: true,
     grabCursor: true,
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets:true,
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
     },
-/*     autoplay: {
+    autoplay: {
         delay: 3000,
         disableOnInteraction: false,
-    },  */
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
     breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 50,
-      },
-      1900: {
-        slidesPerView: 4,
-        spaceBetween: 80,
-      },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+        1240: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+        },
+        1900: {
+            slidesPerView: 7,
+            spaceBetween: 40,
+        },
     },
-  });
+});
